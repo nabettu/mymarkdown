e72fef8d3bb679848dd76bfcc7c3fb6f7c298b23
nabettu <tatsuaki.w@gmail.com>
2018-01-15 23:36:43 +0900

    HOMEとEDITERのコンポーネント作成

---
+++ src/components/Editer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="editer">
+    <h1>エディター画面</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'editer',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="home">
+    <h1>{{ msg }}</h1>
+    <a>Googleアカウントでログイン</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Welcome to MyMarkdown'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
