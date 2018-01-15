352699df8adc012138f1ec9f04b1a0fdaa601858
nabettu <tatsuaki.w@gmail.com>
2018-01-16 00:54:42 +0900

    ユーザーデータを表示

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
 <div id="app">
   <Home v-if="!isLogin"></Home>
-  <Editer v-if="isLogin"></Editer>
+  <Editer v-if="isLogin" :user="userData"></Editer>
 </div>
 </template>
 
@@ -13,7 +13,8 @@ export default {
   name: 'app',
   data() {
     return {
-      isLogin: false
+      isLogin: false,
+      userData: null,
     }
   },
   mounted: function() {
@@ -21,8 +22,10 @@ export default {
       console.log(user);
       if (user) {
         this.isLogin = true;
+        this.userData = user;
       } else {
         this.isLogin = false;
+        this.userData = null;
       };
     });
   },
--- src/components/Editer.vue
+++ src/components/Editer.vue
@@ -1,14 +1,21 @@
 <template>
-  <div id="editer">
-    <h1>エディター画面</h1>
-  </div>
+<div id="editer">
+  <h1>エディター画面</h1>
+  <span>{{ user.displayName }}</span>
+  <button @click="logout">ログアウト</button>
+</div>
 </template>
 
 <script>
 export default {
   name: 'editer',
-  data () {
-    return {
+  props: ['user'],
+  data() {
+    return {}
+  },
+  methods: {
+    logout: function() {
+      firebase.auth().signOut();
     }
   }
 }
