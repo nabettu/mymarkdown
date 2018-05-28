02b54c0a272d3632ee701c7e4783a1f73f589a2d
nabettu <tatsuaki.w@gmail.com>
2018-05-28 23:22:46 +0900

    webkitエラー解消

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import 'shitajicss';
-import './scss/style.scss';
-import Vue from 'vue'
-import App from './App.vue'
+import "shitajicss/docs/css/shitaji.css";
+import "./scss/style.scss";
+import Vue from "vue";
+import App from "./App.vue";
 
 new Vue({
-  el: '#app',
+  el: "#app",
   render: h => h(App)
-})
+});
